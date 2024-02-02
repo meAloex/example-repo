48dde59450daa3c023a21da94affec7de20831d7
Aloex <me@aloex.dev>
2024-02-02 06:49:11 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,3 +3,7 @@ import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
 })
+
+EventsSDK.on("GameStarted", () => {
+	console.log("AGa!")
+})
